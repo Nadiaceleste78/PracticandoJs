fc810df6a8f6c189265d848db5771c5d1b2a4602
Nadia <ferreyranadia78@gmail.com>
2020-08-05 12:49:15 -0300

    suma de números

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-let formulario = document.querySelector("#registro");
+// *********************
+// EJERCICIO 1
+// *********************
 
-formulario.addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    let campos = formulario.querySelectorAll("input:not([type=submit])");
-    let errores = formulario.querySelectorAll(".mensajeError");
-    
-    for (let error of errores) {
-        formulario.removeChild(error);
-    }
-    
-    for (let campo of campos) {
-        let mensaje = document.createElement("p");
-        mensaje.classList.add("mensajeError");
-        
-        if (campo.value == "") {
-            mensaje.innerHTML = "Campo vacío";
-            campo.after(mensaje);
-        }
+// Dado un array de números enteros, devolver la suma de todos los elementos.
 
-        function validarTelefono(){
-            let telefono = prompt ("ingrese su número de teléfono");
-            telefono = parseInt(telefono);
+// Pistas:
+// 1 - Los números ya son de tipo number, así que no es necesario parsearlos
+// 2 - Es necesario usar un ciclo que nos permita iterar sobre cada item del array
 
-            if(isNaN(telefono)){
-                console.log ("El dato ingresado es inválido");
-                return alert ("El dato ingresado es invalido");
-            }
-                let telefonString = telefonString();
-            if (telefonString.length !=11 && telefonString.length != 13) {
-                console.log("El dato es inválido");
-                return alert ("Ingrese nuevamente su número de teléfono");
-                }
+function sumarElementos(numeros) {
+  let suma = 0;
+  
+  let mensaje = document.querySelector("#mensaje");
+  // Generar la lógica acá
+  
+  for (let i = 0; i <numeros.length; i++);
 
-                alert ("El dato ingresado es correcto")
-        }
-        validarTelefono();
-    }
+    if (suma += numeros [i]);{
+      
+    console.log( `La suma de los números es: ${suma}`)}
 
-});
+  return suma;
+}
+
+let numeros = [1, 2, 3];
+
+// Ejecutamos la función e imprimimos el resultado en la consola.
+//console.log(sumarElementos(numeros));
